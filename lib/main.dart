bc11689a34f96873d794e8d1caa43cd1f32f0852
Alan21103 <syahlansaputra03@gmail.com>
2025-05-15 23:22:22 +0700

    refactor menjadi home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:camera_app/bloc/camera_bloc.dart';
-import 'package:camera_app/camerapage.dart';
+import 'package:camera_app/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
